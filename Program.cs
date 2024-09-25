010e2f8ced1ffba54e94484021c14a861da71820
Duc.NT <311020ntd@gmail>
2024-09-25 16:12:35 +0700

    update

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Circle circle = new Circle(5);
+Rectangle rectangle1 = new Rectangle(3, 7);
+Rectangle rectangle2 = new Rectangle(7, 9);
+Shape[] shapes = new Shape[] { circle, rectangle1, rectangle2 };
+foreach (Shape shape in shapes)
+{
+    if (shape is IColorable)
+    {
+        ((IColorable)shape).HowToColor();
+    }
+   Console.WriteLine(circle.getArea());
+}
